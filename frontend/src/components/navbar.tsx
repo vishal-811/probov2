b31db4356d9d9aea78a67443761c44dabfe4b435
vishalsharma <vishalssharma811@gmail.com>
2025-02-14 13:01:01 +0530

    Update navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import logo from "../assets/logo.avif";
 import { useNavigate } from "react-router-dom";
 import CreateUserPage from "./CreateUser";
 import axios from "axios";
